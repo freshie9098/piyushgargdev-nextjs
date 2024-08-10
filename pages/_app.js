9a611249c2deeaa35b5319dd9ffe835ad57dccee
Saksham <skshmsengar@gmail.com>
2024-08-10 16:47:15 +0530

    fixed issue

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import Layout from "../components/Layout/Layout";
 import "../styles/external.css";
 import "../styles/globals.css";
 
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
